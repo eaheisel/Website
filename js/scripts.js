bf9b33e3cf518925c3e6c0ef7f0b328788f47f45
eaheisel <eaheisel@ncsu.edu>
2022-04-14 22:22:03 -0400

    Java Post

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,29 @@ $(document).ready(function(){
         $("h2").text("Hello, "+$("#name").val()+"!");
     });
 });
+$(document).ready(function(){
+    $("form").submit(function(){
+      event.preventDefault();
+      var toSubmit = {};
+      toSubmit.email = $("#email").val();
+      toSubmit.usr = $("#usr").val();
+      toSubmit.moreInformation = $("#moreInformation").val();
+      toSubmit.message = $("#message").val();
+      $.ajaxSetup({
+        headers: {
+          'Accept': "application/json",
+        }
+      });
+      
+      var form_submit = $.post("https://formspree.io/f/xwkyrqbk", toSubmit);
+      
+      form_submit.done(function(){
+        $("#form-success").modal('show');
+        $("#email").val("");
+        $("#usr").val("");
+        $("#moreInformation").val("");
+        $("#message").val("");
+      });
+      
+    })
+  });
